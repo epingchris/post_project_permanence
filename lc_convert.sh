593e283f890d812c951ec295b1a187dc96ce11d9
E-Ping Rau <epingchris@gmail.com>
2023-02-27 18:54:01 +0000

    completion of last commit

--- lc_convert.sh
+++ lc_convert.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-gdalwarp -of Gtiff -co COMPRESS=LZW -co TILED=YES -ot Byte -te -180.0000000 -90.0000000 180.0000000 90.0000000 -tr 0.002777777777778 0.002777777777778 -t_srs EPSG:4326 NETCDF:C3S-LC-L4-LCCS-Map-300m-P1Y-1992-v2.1.1.nc:BandName lc1992.tif
+conda activate gdal_env
+gdalwarp -of Gtiff -co COMPRESS=LZW -co TILED=YES -ot Byte -te -180.0000000 -90.0000000 180.0000000 90.0000000 -tr 0.002777777777778 0.002777777777778 -t_srs EPSG:4326 NETCDF:ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992-v2.0.7cds.nc:lccs_class lc1992.tif
+gdalwarp -s_srs "EPSG:4326" -t_srs "EPSG:3857" lc1992.tif lc1992_3857.tif
--- paddd.r
+++ paddd.r
@@ -1,11 +1,46 @@
 library(terra)
+library(netcdf)
+#load PADDD Events dataset
+paddd_whole = read.csv("PADDDEvents.csv", header = T) #4962
 
-paddd_whole = read.csv("./PADDDtracker_DataReleaseV2.1_2021/Data/PADDDEvents.csv", header = T) #4962
-paddd_sel = subset(paddd_whole, GeoDataTyp == "Polygon" & Location_K == "Y" & YearPADDD >= 1990 & Marine == 0) #3273
+polyg = terra::vect("./PADDD_Shapefiles/PADDDtracker_DataReleaseV2_1_2021_Poly.shp")
 
-polyg = terra::vect("./PADDDtracker_DataReleaseV2.1_2021/Data/Shapefiles/PADDDtracker_DataReleaseV2_1_2021_Poly.shp")
+# find centroid of polygons
 centr = terra::centroids(polyg)
 centr_df = as.data.frame(terra::geom(centr))
+
+#filter out events outside of the tropics
 centr_trop = subset(centr_df, y <= 2613000 & y >= -2613000) #distance between equator and tropics in meter; 894
-paddd_trop = paddd_sel[paddd_sel$PADDD_ID %in% centr[centr_trop$geom, ]$PADDD_ID, ]
+
+#select and save events with polygon and location, after 1990 (JRC-TMF data available), are terrestrial, and tropical
+paddd_trop = subset(paddd_whole, GeoDataTyp == "Polygon" & Location_K == "Y" & YearPADDD >= 1990 & Marine == 0 &
+                   PADDD_ID %in% centr[centr_trop$geom, ]$PADDD_ID) #792
 write.table(paddd_trop, "paddd_tropical.csv", sep = ",", row.names = F)
+
+polyg_trop = polyg[polyg$PADDD_ID %in% paddd_trop$PADDD_ID] 
+terra::writeVector(polyg_trop, "paddd_tropical.shp", overwrite = T)
+
+#load ESA CCI LC dataset and create vegetation mask
+lc = terra::rast("lc1992_3857.tif")
+
+a = Sys.time()
+lc_vegetation = lc %in% c(30, 40, 50, 60, 70, 80, 90, 100, 110)
+b = Sys.time()
+b - a
+
+terra::writeRaster(lc_vegetation, "lc_vegetation1992.tif", overwrite = T)
+
+#calculate proportion of project area that is vegetation land cover in 1992
+area_vegetation = terra::extract(lc_vegetation, polyg_trop, fun = sum)
+area_tot = terra::extract(lc_vegetation, polyg_trop, fun = length)
+
+area_df = merge(area_vegetation, area_tot, by = "ID", all = T)
+colnames(area_df) = c("ID", "area_vegetation", "area_tot")
+area_df$proportion = area_df$area_vegetation / area_df$area_tot
+
+#select and save events with projects having forest land cover as of 1992 (using ESA CCI LC dataset)
+polyg_sel = polyg_trop[polyg_trop$OBJECTID %in% subset(area_df, proportion >= 0.5)$ID] #113
+terra::writeVector(polyg_sel, "paddd_selected.shp", overwrite = T)
+
+paddd_sel = subset(paddd_trop, PADDD_ID %in% polyg_sel$PADDD_ID) #113
+write.table(paddd_sel, "paddd_selected.csv", sep = ",", row.names = F)
